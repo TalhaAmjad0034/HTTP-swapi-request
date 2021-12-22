7d2f4c1c4f05ad65c352c56e55f4043e7ab11c9c
Talha Amjad <talha.amjad@tecaudex.com>
2021-12-22 17:48:36 +0500

    Added axios method too...

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 import Movies from "./Components/Movies";
 import "./App.css";
@@ -7,19 +8,40 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
+
+
   async function fetchMoviesHandler() {
     setError(false);
     setLoading(true);
 
-    try {
-      const response = await fetch("https://swapi.dev/api/films/");
+    // via try catch
 
-      if (!response.ok) {
-        throw new Error("Something went wrong!");
-      }
+    // try {
+    //   const response = await fetch("https://swapi.dev/api/films/");
 
-      const data = await response.json();
-      const transform = data.results.map((movieData) => {
+    //   if (!response.ok) {
+    //     throw new Error("Something went wrong!");
+    //   }
+
+    //   const data = await response.json();
+    //   const transform = data.results.map((movieData) => {
+    //     return {
+    //       id: movieData.episode_id,
+    //       title: movieData.title,
+    //       para: movieData.opening_crawl,
+    //     };
+    //   });
+    //   setMovies(transform);
+    // } catch (error) {
+    //   setError(error.message);
+    // }
+
+
+    // via axios
+    
+    axios("https://swapi.dev/api/films/")
+    .then(function (response) {
+      const transform = response.data.results.map((movieData) => {
         return {
           id: movieData.episode_id,
           title: movieData.title,
@@ -27,9 +49,13 @@ function App() {
         };
       });
       setMovies(transform);
-    } catch (error) {
+    })
+    .catch(function (error) {
       setError(error.message);
-    }
+    });
+
+
+
     setLoading(false);
   }
 
@@ -42,6 +68,13 @@ function App() {
     content = "Loading...";
   }
 
+axios("https://swapi.dev/api/films/")
+.then(function (response) {
+  console.log(response);
+})
+.catch(function (error) {
+  console.log(error);
+});
   return (
     <div>
       <section className="button">
